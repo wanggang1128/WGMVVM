14fb22963a9f27e030b7bd6a3a012035f30313aa
王刚 <wanggang@317hu.com>
2018-10-12 15:27:06 +0800

    Initial Commit

---
+++ WGMVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGMVVM
+//
+//  Created by wanggang on 2018/10/12.
+//  Copyright © 2018 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGMVVM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGMVVM
+//
+//  Created by wanggang on 2018/10/12.
+//  Copyright © 2018 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
